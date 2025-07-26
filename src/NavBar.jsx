76baf6e761de1d15ca84503c3d5b9fa1e9c819bd
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-26 15:49:29 +0500

    Learn Routes

---
+++ src/NavBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router";
+import './Styles/header.css'
+
+function NavBar(){
+    return(
+        <div className="header">
+        <div>
+            <Link className="link" to='/'><h2>Logo</h2></Link>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <Link to='/' className="link">Home</Link>
+                </li>
+                <li>
+                    <Link to='/about' className="link">About</Link>
+                </li>
+                <li>
+                    <Link to='/login' className="link">Login</Link>
+                </li>
+            </ul>
+        </div>
+        </div>
+    )
+}
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Styles/index.css'
-import App from './App.jsx'
+// import './Styles/index.css'
+// import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import LearnRoutes from './LearnRoutes.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root')).render( 
+  <BrowserRouter>
+      <LearnRoutes />
+  </BrowserRouter>
 )
