50be4cdb3bfd1cbcccd43d5fecdae9dfccbd4514
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-27 01:47:44 +0500

    Learn How to delete data from API

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -4,21 +4,33 @@ import './Styles/API.css'
 export default function UserList() {
 
     const [userData, setuserData] = useState([]);
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
+    const url = "http://localhost:3000/users";
 
 
     useEffect(() => {
-        setLoading(true);
-        getUserData();
+        getUserData(); setLoading(true);
+
     }, [])
 
     async function getUserData() {
-        const url = "http://localhost:3000/users";
         let response = await fetch(url);
         response = await response.json();
         setuserData(response);
         setLoading(false);
     }
+
+    const deleteuser = async (id) => {
+        let response = await fetch(url + '/' + id, {
+            method: 'delete'
+        });
+        response = await response.json();
+        if (response) {
+            alert("Data Deleted")
+            getUserData();
+        }
+    }
+
 
     return (
         <>
@@ -26,18 +38,20 @@ export default function UserList() {
                 <li>Name</li>
                 <li>Email</li>
                 <li>Age</li>
+                <li>Action</li>
             </ul>
 
             {
-                !loading?
-                userData.map((user, index) => (
-                    <ul key={index} className="user-list">
-                        <li>{user.name}</li>
-                        <li>{user.email}</li>
-                        <li>{user.age}</li>
-                    </ul>
-                ))
-                : <h1>Data Loading...</h1>
+                !loading ?
+                    userData.map((user, index) => (
+                        <ul key={index} className="user-list">
+                            <li>{user.name}</li>
+                            <li>{user.email}</li>
+                            <li>{user.age}</li>
+                            <li><button onClick={() => deleteuser(user.id)}>Delete</button></li>
+                        </ul>
+                    ))
+                    : <h1>Data Loading...</h1>
             }
 
         </>
