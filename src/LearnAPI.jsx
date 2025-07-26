822c1fb70ecc9b5c6c2f19a442ce07a6ec6e2745
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-27 00:54:53 +0500

    Make API and Fetch data from it

--- src/LearnAPI.jsx
+++ src/LearnAPI.jsx
@@ -4,36 +4,41 @@ import './Styles/API.css'
 export default function LearnAPI() {
 
     const [userData, setuserData] = useState([]);
+    const [loading,setLoading]=useState(false);
 
 
     useEffect(() => {
+        setLoading(true);
         getUserData();
     }, [])
 
     async function getUserData() {
-        const url = "https://dummyjson.com/users";
+        const url = "http://localhost:3000/users";
         let response = await fetch(url);
         response = await response.json();
-        setuserData(response.users);
+        setuserData(response);
+        setLoading(false);
     }
 
     return (
         <>
             <h1>Fetch Data From API</h1>
             <ul className="user-list user-list-head">
-                <li>FirstName</li>
-                <li>LastName</li>
+                <li>Name</li>
+                <li>Email</li>
                 <li>Age</li>
             </ul>
 
             {
+                !loading?
                 userData.map((user, index) => (
                     <ul key={index} className="user-list">
-                        <li>{user.firstName}</li>
-                        <li>{user.lastName}</li>
+                        <li>{user.name}</li>
+                        <li>{user.email}</li>
                         <li>{user.age}</li>
                     </ul>
                 ))
+                : <h1>Data Loading...</h1>
             }
 
         </>
