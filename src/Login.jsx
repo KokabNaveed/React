62a9450fb817aea3f607cf0e1e7191105f326f69
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2025-07-28 15:48:37 +0500

    Validation with UseActionState Hook

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useActionState, useState } from "react";
 import './Styles/API.css';
 
 function Login() {
+
+    // this all is for simple validation
 
     const [name, setName] = useState('');
     const [nameErr, setNameErr] = useState();
@@ -34,19 +36,63 @@ function Login() {
         }
     }
 
+    // this all is for useActionState Hook validation
+
+    const handleLogin = (prevData, formData) => {
+
+        let name = formData.get('name');
+        let password = formData.get('password');
+
+        let regex = /^[A-Z0-9]+$/i;
+
+
+        if (!name || name.length > 5) {
+            return { error: 'Please Enter valid username. 5 character are allowed' ,name,password}
+        } else if (!password || !regex.test(password)) {
+            return { error: 'Please Enter valid Password. Only character and numbers are allowed',name,password }
+        }
+        else {
+            return { message: 'Login Successfully',name,password }
+        }
+    }
+
+    const [data, action, pending] = useActionState(handleLogin)
+
 
     return (
         <div style={{ textAlign: 'center' }}>
-            <h1>Login Page</h1>
-            <input type="text" className={nameErr ? 'error' : ''} onChange={handleName} placeholder="Enter Name" />
-            <br />
-            <span className="red-color">{nameErr && nameErr}</span>
-            <br />
-            <input type="text" className={passwordErr ? 'error' : ''} onChange={handlePassword} placeholder="Enter Password" />
-            <br />
-            <span className="red-color">{passwordErr && passwordErr}</span>
-            <br />
-            <button disabled={nameErr || passwordErr}>Login</button>
+
+            <div>
+                <h2>Simple Validation Login Page</h2>
+                <input type="text" className={nameErr ? 'error' : ''} onChange={handleName} placeholder="Enter Name" />
+                <br />
+                <span className="red-color">{nameErr && nameErr}</span>
+                <br />
+                <input type="text" className={passwordErr ? 'error' : ''} onChange={handlePassword} placeholder="Enter Password" />
+                <br />
+                <span className="red-color">{passwordErr && passwordErr}</span>
+                <br />
+                <button disabled={nameErr || passwordErr}>Login</button>
+            </div>
+
+            <div>
+                <h2>Validation With UseActionState</h2>
+                {
+                    data?.message && <span style={{color:'green'}}> {data?.message} </span>
+                }
+
+                {
+                    data?.error && <span style={{color:'red'}}> {data?.error} </span>
+                }
+
+                <form action={action}>
+                    <input type="text" defaultValue={data?.name} name="name" placeholder="Enter Name" />
+                    <br /><br />
+                    <input type="text" defaultValue={data?.password} name="password" placeholder="Enter Password" />
+                    <br /><br />
+                    <button>Login</button>
+                </form>
+            </div>
         </div>
 
     )
